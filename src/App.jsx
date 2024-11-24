2eba27d87a30c107aa8acad6370f63bc20a8df26
Ayush Kumar <aayushkumar1228@gmail.com>
2024-11-25 02:26:30 +0530

    download assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <h1 className='font-bold bg-red-600 font-mono'>Welcome to the Redefine App!</h1>
+      <p>This is a sample React application.</p>
+    </main>
   )
 }
 
